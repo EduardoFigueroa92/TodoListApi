3f2bd87b7ad63ee438181a335777d219518e6b64
Eduardo Figueroa <156800901+EduardoFigueroa92@users.noreply.github.com>
2024-04-21 09:42:43 +0000

    faltan PUT y DELETE tareas

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,47 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useEffect, useState } from "react";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+    const [todoList, setTodoList] = useState([]); 
+
+    useEffect(() => {
+        getTodos(); // Llamamos a la función getTodos al montar el componente
+    }, []);
+
+    const getTodos = () => {
+        fetch("https://playground.4geeks.com/todo/users/edu") 
+            .then(response => response.json())
+            .then(result => {
+                console.log(result);
+                setTodoList(result.todos); // Guardamos la lista de tareas en el estado
+            })
+            .catch(error => console.error(error));
+    };
+
+    return (
+        <div className="container">
+            <h1>Lista de tareas pendientes</h1>
+			{/* Aquí va la funcionalidad para añadir tareas */}
+            <ul>
+                {todoList.map((item, index) => (
+                    <li>
+                        {item.label} {/* Mostramos el título de la tarea */}
+                        <i
+                            className="fas fa-trash-alt float-end"
+                            onClick={() =>
+                                setTodoList(prevList =>
+                                    prevList.filter(
+                                        (t, currentIndex) => index !== currentIndex
+                                    )
+                                )
+                            } // Eliminamos la tarea al hacer clic en el icono de la papelera
+                        ></i>
+                    </li>
+                ))}
+            </ul>
+            <div>{todoList.length} tareas</div> {/* Mostramos la cantidad de tareas */}
+        </div>
+    );
 };
 
 export default Home;
